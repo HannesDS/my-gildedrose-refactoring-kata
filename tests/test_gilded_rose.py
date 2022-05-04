b99b91f92739dc9846aadf8be687ac9244c6dff2
Hannes De smet <hannesdesmet@Hanness-MacBook-Pro.local>
2022-05-04 13:41:38 +0200

    feat: scafold some new tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -9,8 +9,36 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+
+    def test_update_quality_basic_item_negative_sell_in(self):
+        quality = 10
+        items = [Item("basic", -1, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(quality - 2, items[0].quality)
+
+    def test_update_quality_basic_item_positive_sell_in(self):
+        pass
+
+    def test_update_quality_basic_item_quality_min(self):
+        pass
+
+    def update_quality_basic_item_quality_max(self):
+        pass
+
+    def update_quality_aged_brie(self):
+        pass
+
+    def update_quality_sulfuras(self):
+        pass
+
+    def update_quality_backstage_pass_negative_sell_in(self):
+        pass
+
+    def update_quality_backstage_pass_positive_sell_in(self):
+        pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
